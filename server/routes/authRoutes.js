3d83794aecc91f55ef79f90a197aada25bf28068
Aldrich L Suba <aldrichsuba09@gmail.com>
2025-07-17 20:39:23 +0800

    Added admin pages

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,5 +5,12 @@ const { register, login, logout} = require("../controllers/authController")
 router.post("/register", register)
 router.post("/login", login)
 router.post("/logout", logout)
+router.get("/me", (req, res) => {
+  if (req.session.user) {
+    res.json({ success: true, user: req.session.user });
+  } else {
+    res.status(401).json({ success: false, message: "Not logged in" });
+  }
+});
 
 module.exports = router;
